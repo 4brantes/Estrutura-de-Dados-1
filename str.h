1dcb4d3a997af1b8e7ff517ec19e72a80dfc5a74
Guilherme Abrantes <guilherme.jkl.rn28@gmail.com>
2024-08-01 11:36:49 -0300

    delet

--- str.h
+++
@@ -1,7 +0,0 @@
-/* funções oferecidas pelo modulo srt.c*/
-/* função comprimento ** retorna o número de caracteres da string passada como parâmetro*/
-int comprimento (char*str);
-/*função copia** copia os caracteres da string orig(origem) para dest (destino)*/
-void copia (char* dest, char* orig);
-/*funcao concatena** Concatena a string orig (origem) na string dest (destino)*/
-void concatena (char* dest, char*orig);
